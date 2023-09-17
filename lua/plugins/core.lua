5e81ef77b7688292a3968d9f5a40700a4cc4f896
Saksham Gupta <sakshamgupta05@gmail.com>
2023-09-17 11:05:50 +0530

    Replace surround plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -96,7 +96,16 @@ return {
   -- End certain structures
   { "tpope/vim-endwise" },
   -- Quoting / parenthesizing
-  { "tpope/vim-surround" },
+  {
+    "kylechui/nvim-surround",
+    version = "*",   -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
   -- Auto matching for brackets
   {
     'windwp/nvim-autopairs',
