b19f54c3ce0a194975eef25bb291db544b5482c4
Saksham Gupta <sakshamgupta05@gmail.com>
2024-10-11 02:08:04 +0530

    Add golang support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -125,4 +125,18 @@ return {
   -- }
   -- NOTE: For project level settings
   -- https://github.com/folke/neoconf.nvim
+  -- {
+  --   "ray-x/go.nvim",
+  --   dependencies = { -- optional packages
+  --     "ray-x/guihua.lua",
+  --     "neovim/nvim-lspconfig",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  --   config = function()
+  --     require("go").setup()
+  --   end,
+  --   event = { "CmdlineEnter" },
+  --   ft = { "go", 'gomod' },
+  --   build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  -- }
 }
